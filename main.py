32a2ce4ffc8f59c56a18de2a739ce9bd1648fee8
siarasmall <41011312+siarasmall@users.noreply.github.com>
2025-03-10 12:59:04 -0400

    Scaffolding for plotting module; change adjacency matrix representation from two dimensional array to dictionary; clean up and document code

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ def main():
     for timestep in range(0, 40, 1):
         flips = algo.metropolis(timestep, 400)
         print(f"RUNNING METROPOLIS ALGO WITH {timestep} TIMESTEPS. FLIPS = {flips}")
-    # print("Lattice:")
-    # algo.lattice.print_test()
-    # print("Adj matrix:")
-    # algo.adj_matrix.print_test()
-    # print("Lattice:")
-    # algo.lattice.print_test()
-    # print("Adj matrix:")
-    # algo.adj_matrix.print_test()
 
 if __name__ == "__main__":
     main()
--- metropolis.py
+++ metropolis.py
@@ -1,25 +1,49 @@
-import numpy as np
 from ising import Ising
 from random import randrange, uniform
 from math import e
 import scipy.constants
 
 class Metropolis(Ising):
+    """
+    Class for the Metropolis algorithm for the Ising model of ferromagnetism.
+    """
     def __init__(self, width: int, height: int) -> None:
+        """
+        Arguments:
+            - width: int = Width of the lattice. 
+            - width: int = Height of the lattice. 
+        """
         super().__init__(width, height)
 
-    def metropolis(self, time_steps: int, temp):
+    def metropolis(self, time_steps: int, temp) -> int:
+        """
+        Performs the Metropolis algorithm.
+
+        Arguments:
+            - time_steps: int = Number of time steps to run the algorithm.
+            - temp: int = Temperature in K.
+
+        Returns:
+            Integer representing the number of successful flips during 
+            the running of the algorithm.
+        """
+        # Accumulate sum of successful flips
         flips = 0
         for t in range(time_steps):
+            # Get random site in lattice
             i = randrange(0, self.N)
+            # Calculate original energy
             old_E = self.hamiltonian(i)
+            # Perform random flip
             self.lattice.flip(i)
+            # Calculate new energy
             new_E = self.hamiltonian(i)
+            # Calculate change in energy
             delta_E = old_E - new_E
+            # Analyze if flip should be kept; if not, flip back
             if delta_E >= 0 or uniform(0, 1) >= pow(e, (-delta_E/ (scipy.constants.k*temp))):
-            # if delta_E >= 0:
                 self.lattice.flip(i)
-                # print(f"keeping flip. detla e = {delta_E}. old E = {old_E} and new E = {new_E}")
             else:
+                # If flip is kept, increment flip sum
                 flips = flips + 1
         return flips
